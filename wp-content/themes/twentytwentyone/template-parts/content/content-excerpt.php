ac54a1abcda7bc8d79e21ceabe1afe6fa2ec3956
tranvuxuanloc26 <21211tt4875@mail.tdc.edu.vn>
2023-11-01 11:25:06 +0700

    commit

--- wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
@@ -13,37 +13,60 @@
 
 
 <div class="list-new-view">
-<article id="post-<?php the_ID(); ?>" <?php post_class();?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <div class="row top_news_block_desc">
-
-	<?php					
-	$post = get_post();					
-	$date = $post->post_date;					
-	$day = date("j", strtotime($date));					
-	$month = date("F", strtotime($date));					
+    <?php
 	
-	$post_date = get_the_date('d', $post->ID);					
-	$post_month = get_the_date('m', $post->ID);		
-	$post_date = get_the_date('d', $post->ID);					
-	$post_month = get_the_date('m', $post->ID);			
-	
-	
-	echo '<div class="col-md-3 topnewstime">';
-	echo '<span class="topnewsdate">'. $day .'</span>';
-	echo '<br>';
-	echo '<span class="topnewsmonth">'. "Tháng ". $post_month .'</span>';
-	echo '</div>';
-	 ?>
-	 <div class="col-md-9 shortdesc">
-	<?php get_template_part( 'template-parts/header/excerpt-header', get_post_format() ); ?>
-
-	<div class="entry-content">
-		<?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?>
-	</div><!-- .entry-content -->
-	</div>
-	<!--<footer class="entry-footer default-max-width">
-		<?php twenty_twenty_one_entry_meta_footer(); ?>
-	</footer> .entry-footer -->
-	</div>
+    if (!is_home()) {
+        $post = get_post();
+        $date = $post->post_date;
+        $day = date("j", strtotime($date));
+        $month = date("F", strtotime($date));
+    
+        $post_date = get_the_date('d', $post->ID);
+        $post_month = get_the_date('m', $post->ID);
+        $post_date = get_the_date('d', $post->ID);
+        $post_month = get_the_date('m', $post->ID);
+    ?>
+    <div class="col-md-3 image-post">
+        <?php the_post_thumbnail('thumbnail', array('class' => 'post-thumbnail')); ?>
+    </div>
+    <div class="col-md-3 topnewstime">
+        <span class="topnewsdate"><?php echo $day; ?></span><br>
+        <span class="topnewsmonth"><?php echo "Tháng " . $post_month; ?></span>
+    </div>
+    <div class="col-md-6 shortdesc">
+        <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <div class="entry-content">
+            <?php get_template_part('template-parts/excerpt/excerpt', get_post_format()); ?>
+        </div><!-- .entry-content -->
+    </div>
+    <?php
+    } else {
+        $post = get_post();
+        $date = $post->post_date;
+        $day = date("j", strtotime($date));
+        $month = date("F", strtotime($date));
+    
+        $post_date = get_the_date('d', $post->ID);
+        $post_month = get_the_date('m', $post->ID);
+        $post_date = get_the_date('d', $post->ID);
+        $post_month = get_the_date('m', $post->ID);
+    ?>
+    <div class="col-md-3 topnewstime">
+        <span class="topnewsdate"><?php echo $day; ?></span><br>
+        <span class="topnewsmonth"><?php echo "Tháng " . $post_month; ?></span>
+    </div>
+    <div class="col-md-9 shortdesc">
+        <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <div class="entry-content">
+            <?php get_template_part('template-parts/excerpt/excerpt', get_post_format()); ?>
+        </div><!-- .entry-content -->
+    </div>
+    <?php
+    }
+    ?>
+</div>
 </article><!-- #post-${ID} -->
+
 
